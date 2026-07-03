10e08b912cc79aa19d549f78bea4d913ac6b368e
gcm1991 <76789356+gcm1991@users.noreply.github.com>
2026-07-02 20:23:09 -0500

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import childImg from "./assets/child.png";
+import bookImg from "./assets/book.jpg";
+import familyImg from "./assets/family.jpg";
+import heartImg from "./assets/heart.jpg";
+import sunGlassesImg from "./assets/sunglasses.jpg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const input_box_width = "800px";
-  const question_margin_bottom = "10px";
+  const question_margin_top = "10px";
+  const question_margin_bottom = "5px";
+  const question_margin_left = "30px";
+  const title_margin_left = "330px";
 
   const about_questions = [
     "1. What does your child like to be called?",
@@ -39,78 +46,79 @@ function App() {
   ];
 
   const all_questions = [
-    { title: "About", questions: about_questions },
-    { title: "About", questions: learning_questions },
-    { title: "About", questions: about_questions },
-    { title: "About", questions: about_questions },
-    { title: "About", questions: about_questions },
+    { title: "About", image: childImg, questions: about_questions },
+    {
+      title: "Learning",
+      image: bookImg,
+      questions: learning_questions,
+    },
+    {
+      title: "Family",
+      image: familyImg,
+      questions: family_questions,
+    },
+    {
+      title: "Support",
+      image: heartImg,
+      questions: support_questions,
+    },
+    {
+      title: "Just For Fun",
+      image: sunGlassesImg,
+      questions: fun_questions,
+    },
   ];
 
   return (
     <>
       <h1>Hello Parent</h1>
       <div>
-        <h2>About</h2>
-        <div style={{ display: "flex", width: "100%" }}>
-          <img
-            src={childImg}
-            className="base"
-            width="170"
-            height="179"
-            alt=""
-          />
-          <div className="questions">
-            {about_questions.map((question) => (
-              <>
-                {" "}
-                <p style={{ marginBottom: question_margin_bottom }}>
-                  {question}
-                </p>
-                <input style={{ width: input_box_width }} text></input>
-              </>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div>
-        <h2>Learning</h2>
-        <div style={{ display: "flex", width: "100%" }}>
-          <img
-            src={childImg}
-            className="base"
-            width="170"
-            height="179"
-            alt=""
-          />
-          <div className="questions">
-            {learning_questions.map((question) => (
-              <p style={{ marginBottom: question_margin_bottom }}>
-                {question}
-                <input style={{ width: input_box_width }} text></input>
-              </p>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div>
-        <h2>About</h2>
-        <div style={{ display: "flex", width: "100%" }}>
-          <img
-            src={childImg}
-            className="base"
-            width="170"
-            height="179"
-            alt=""
-          />
-          <div className="questions">
-            {about_questions.map((question) => (
-              <p style={{ marginBottom: question_margin_bottom }}>
-                {question}
-                <input style={{ width: input_box_width }} text></input>
-              </p>
-            ))}
-          </div>
-        </div>
+        {all_questions.map((section) => (
+          <>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "left",
+                marginLeft: title_margin_left,
+              }}
+            >
+              <h2 style={{ marginTop: "30px" }}>{section.title}</h2>
+            </div>
+
+            <div style={{ display: "flex", width: "100%" }}>
+              <img
+                src={section.image}
+                className="base"
+                width="200"
+                height="250"
+                alt=""
+              />
+              <div className="questions" style={{ flex: 1 }}>
+                {section.questions.map((question) => (
+                  <>
+                    {" "}
+                    <p
+                      style={{
+                        marginTop: question_margin_top,
+                        marginBottom: question_margin_bottom,
+                        marginLeft: question_margin_left,
+                      }}
+                    >
+                      {question}
+                    </p>
+                    <input
+                      style={{
+                        width: "90%",
+                        marginLeft: question_margin_left,
+                      }}
+                      text
+                    ></input>
+                  </>
+                ))}
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
